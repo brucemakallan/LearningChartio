c476e11ae0d1ce01b3723716d9517aeb589c237c
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-09-05 01:09:02 +0300

    Add Readable dates to Line-chart

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -5,6 +5,7 @@ import {
 import { colors } from '../../utils/colors';
 import { Articles } from '../../utils/interfaces';
 import './Chart.scss';
+import moment from 'moment';
 
 export interface ChartProps {
   articles: Articles;
@@ -17,11 +18,17 @@ const lines = [
   { dataKey: 'comments', stroke: colors.teal, activeDot: { r: 4 } },
 ];
 
+const toReadableDate = (date: string): string => moment(date, 'YYYY-MM-DD').format('MMM Do');
+
 const Chart: React.SFC<ChartProps> = ({ articles }: ChartProps) => {
   const data: object[] = [];
   articles.forEach(article => {
     data.push({
-      date: article[1], readers: article[3], likes: article[4], dislikes: article[5], comments: article[6],
+      date: toReadableDate(article[1]),
+      readers: article[3],
+      likes: article[4],
+      dislikes: article[5],
+      comments: article[6],
     });
   });
 
